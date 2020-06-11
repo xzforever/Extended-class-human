e502ac838000b4946437a37c5e54552ecedac085
xzforever <xzforever.1234@gmail.com>
2020-06-12 02:06:19 +0300

    Added Properties

--- 2article2_3/Human.cs
+++ 2article2_3/Human.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿    using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
@@ -10,58 +10,75 @@ namespace _2article2_3
 {
     class Human
     {
-        private string Name;
-        private string Surname;
-        private int YearOfBirth;
-        private int MonthOfBirth;
-        private int DayOfBirth;
+        private string _name;
+        private string _surname;
+        private int _yearOfBirth;
+        private int _monthOfBirth;
+        private int _dayOfBirth;
         public Human(string name, string surname, int yearOfBirth, int monthOfBirth, int dayOfBirth)
         {
-            SetYear(yearOfBirth);
-            SetMonth(monthOfBirth);
-            SetDay(dayOfBirth);
+            _yearOfBirth = yearOfBirth;
         }
-        public int GetYear()
+        public int YearOfBirth
         {
-            return YearOfBirth;
+            get
+            {
+                return _yearOfBirth;
+            }
+            set
+            {
+                if (value < 1900) _yearOfBirth = 1900;
+                else if (value > 2020) _yearOfBirth = 2020;
+                else _yearOfBirth = value;
+            }
         }
-        public int GetMonth()
+        public int MonthOfBirth
         {
-            return MonthOfBirth;
+
+            get
+            {
+                return _monthOfBirth;
+            }
+            set
+            {
+                if (value < 1) _monthOfBirth = 1;
+                else if (value > 12) _monthOfBirth = 12;
+                else _monthOfBirth = value;
+            }
         }
-        public int GetDay()
+        public int DayOfBirth
         {
-            return DayOfBirth;
+            get
+            {
+                return _dayOfBirth;
+            }
+            set
+            {
+                if (value < 1) _dayOfBirth = 1;
+                else if (value > 31) _dayOfBirth = 31;
+                else _dayOfBirth = value;
+            }
         }
-        public void SetYear(int yearOfBirth)
-        {
-            if (yearOfBirth < 1900) yearOfBirth = 1900;
-            else if (yearOfBirth > 2020) yearOfBirth = 2020;
-            else YearOfBirth = yearOfBirth;
-        }
-        public void SetMonth(int monthOfBirth)
-        {
-            if (monthOfBirth < 1) monthOfBirth = 1;
-            else if (monthOfBirth > 12) monthOfBirth = 12;
-            else MonthOfBirth = monthOfBirth;
-        }
-        public void SetDay(int dayOfBirth)
-        {
-            if (dayOfBirth < 1) dayOfBirth = 1;
-            else if (dayOfBirth > 31) dayOfBirth = 31;
-            else DayOfBirth = dayOfBirth;
-        }
-        public void SetFullName(string fullName, string name, string surname)
+        public void  SetFullName(string fullName, string name, string surname)
         {
             fullName = name + surname;
-            Name = name;
-            Surname = surname;
+            _name = name;
+            _surname = surname;
         }
+        public Human(string fullName)
+        {
+            FullName = fullName;
+        }
+        public string FullName { get; private set; }
         public void SetAge(int age, int yearOfBirth)
         {
             age = 2020 - yearOfBirth;
-            YearOfBirth = yearOfBirth;
         }
+        public Human(int age)
+        {
+            Age = age;
+        }
+        public int Age { get; private set; }
 
 
     }
